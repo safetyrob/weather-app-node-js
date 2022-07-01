d6fbbe95949872c8dc0064cb8e85c0c60bc5b545
safetyrob <robert@safetymts.com>
2022-06-30 18:36:56 -0600

    Update for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,18 +12,16 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading weather...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-          console.log(data.location);
-          console.log(data.forecast);
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
 });
